95dc07691973f11003258b9ca90158f0b3a7f16c
nicolasbondancia <nicolasbondancia@gmail.com>
2017-09-05 19:58:06 -0300

    simplificar TP1
    Simplifique el TP1 poniendo el number directo cuando pido el numero por prompt

---
+++ Trabajo_practico_clase1/ejercicio.js
@@ -0,0 +1,14 @@
+//EJERCICIO TP 1
+
+var numero1 = Number(prompt ("Ingrese el primer numero con el que desea operar ", "aqui"))
+var numero2 = Number(prompt ("Ingrese el segundo numero con el que desea operar ", "aqui"))
+
+resultado1 = numero1 + numero2
+resultado2= numero1 - numero2
+resultado3 = numero1 * numero2
+resultado4 = numero1 / numero2
+
+alert (numero1 + " + " + numero2 + " es igual a: " + resultado1)
+alert (numero1 + " - " + numero2 + " es igual a: " + resultado2)
+alert (numero1 + " * " + numero2 + " es igual a: " + resultado3)
+alert (numero1 + " / " + numero2 + " es igual a: " + resultado4)
